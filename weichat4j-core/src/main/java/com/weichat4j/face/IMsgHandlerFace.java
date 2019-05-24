7abfa77a751b90064d7649e1fa42a8cbcca21f85
yjm <yjm@qq.com>
2019-05-24 17:20:00 +0800

    测试

--- weichat4j-core/src/main/java/com/weichat4j/face/IMsgHandlerFace.java
+++ weichat4j-core/src/main/java/com/weichat4j/face/IMsgHandlerFace.java
@@ -12,9 +12,7 @@ import com.weichat4j.beans.BaseMsg;
  */
 public interface IMsgHandlerFace {
 	/**
-	 * 
-	 * @author https://github.com/yaphone
-	 * @date 2017年4月20日 上午12:15:00
+	 * 处理文本消息
 	 * @param msg
 	 * @return
 	 */
@@ -22,9 +20,6 @@ public interface IMsgHandlerFace {
 
 	/**
 	 * 处理图片消息
-	 * 
-	 * @author https://github.com/yaphone
-	 * @date 2017年4月21日 下午11:07:06
 	 * @param msg
 	 * @return
 	 */
@@ -32,9 +27,6 @@ public interface IMsgHandlerFace {
 
 	/**
 	 * 处理声音消息
-	 * 
-	 * @author https://github.com/yaphone
-	 * @date 2017年4月22日 上午12:09:44
 	 * @param msg
 	 * @return
 	 */
@@ -42,9 +34,6 @@ public interface IMsgHandlerFace {
 
 	/**
 	 * 处理小视频消息
-	 * 
-	 * @author https://github.com/yaphone
-	 * @date 2017年4月23日 下午12:19:50
 	 * @param msg
 	 * @return
 	 */
@@ -52,9 +41,6 @@ public interface IMsgHandlerFace {
 
 	/**
 	 * 处理名片消息
-	 * 
-	 * @author https://github.com/yaphone
-	 * @date 2017年5月1日 上午12:50:50
 	 * @param msg
 	 * @return
 	 */
@@ -62,18 +48,12 @@ public interface IMsgHandlerFace {
 
 	/**
 	 * 处理系统消息
-	 * 
-	 * @author Relyn
-	 * @date 2017年6月21日17:43:51
 	 * @param msg
-	 * @return
 	 */
 	public void sysMsgHandle(BaseMsg msg);
 
 	/**
 	 * 处理确认添加好友消息
-	 * 
-	 * @date 2017年6月28日 下午10:15:30
 	 * @param msg
 	 * @return
 	 */
@@ -81,8 +61,6 @@ public interface IMsgHandlerFace {
 
 	/**
 	 * 处理收到的文件消息
-	 * 
-	 * @date 2017年7月21日 下午11:59:14
 	 * @param msg
 	 * @return
 	 */
